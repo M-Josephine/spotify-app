5291bc96b6c2174579f532fac21f85a44a8b60ee
m-josephine <mry.josephine@gmail.com>
2025-08-08 18:00:53 +0200

    Add player + esthetics

--- app.py
+++ app.py
@@ -12,44 +12,66 @@ import streamlit.components.v1 as components
 ############################### Header ########################################
 
 st.title("Pimp my track :sunglasses:")
+st.write('The recommended track will be generated based on your Spotify top tracks, as well as song attributes such as danceability or popularity, that you will be able to tune a little further.')
 
 ############################### GET TOP TRACKS ########################################
 
 # Get user top tracks
 
+# Authenticate to Spotify API
 def api_spotify_auth(scope):
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=st.secrets.spotify_api_credentials.client_id, client_secret= st.secrets.spotify_api_credentials.client_secret, redirect_uri=st.secrets.spotify_api_credentials.redirect_url, scope=scope))
     return sp
 
+# Get 5 top tracks
 def get_top_tracks(time_range, track_nb, offset, sp):
 
-    # API result
+    # API response
     top_tracks = sp.current_user_top_tracks(limit=track_nb, offset=offset, time_range=time_range)
     
     # Create dataframe and list of ids
     track_details = []
     track_number = len(top_tracks["items"])
     track_ids = []
-    print(track_number)
+    #print(track_number)
 
     for i in range(track_number):
         track_name = top_tracks['items'][i]['name']
         track_artists = top_tracks['items'][i]['artists'][0]['name']
-        track_url = top_tracks['items'][i]['external_urls']['spotify']
         track_id = top_tracks['items'][i]['id']
+        
 
         track_details.append({
                     "Title": track_name,
                     "Artist": track_artists,
-                    "URL": track_url
-                    #"ID" : track_id
+                    "ID" : track_id
                 })
+        
         track_ids.append(track_id)
+        
 
     df_tracks = pd.DataFrame(track_details)
     
-    #replace by st.dataframe
-    st.dataframe(df_tracks, hide_index=True)
+    # Display the track details
+    # st.dataframe(df_tracks, hide_index=True)
+
+    # Display the top tracks. As we want to display the player, we can't use st.dataframe here
+    #col1, col2, col3 = st.columns([0.3, 0.3, 0.8])
+    #col1.markdown("**Title**")
+    #col2.markdown("**Artist**")
+    st.markdown("**Your top tracks**")
+    
+    for index, row in df_tracks.iterrows():
+        # col1, col2, col3 = st.columns([0.3, 0.3, 0.8])
+        # col1.markdown(row['Title'])
+        # col2.markdown(row['Artist'])
+        
+        embed_code = f"""
+            <iframe src="https://open.spotify.com/embed/track/{row['ID']}" width="100%" height="80" 
+                    frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        """
+        #with col3:
+        components.html(embed_code, height=100)
     
     return track_ids
 
@@ -61,17 +83,30 @@ def get_top_tracks(time_range, track_nb, offset, sp):
 def build_params(seeds):
     
     # Define param for each feature with widgets
-    #0 to 1
+    # Features with values of 0 to 1
     st.header("Now, tune it your way", divider = 'green')
     enable_sliders = st.toggle("Select track features")
     if enable_sliders:
-        acousticness = st.slider("Acousticness :", 0.0, 1.0, 0.2)
-        danceability = st.slider("Danceability :", 0.0, 1.0, 0.2)
-        energy = st.slider("Energy :", 0.0, 1.0, 0.2)
-        instrumentalness = st.slider("Instrumentalness :", 0.0, 1.0, 0.2)
-        liveness = st.slider("Liveness :", 0.0, 1.0, 0.2)
-        speechiness = st.slider("Speechiness :", 0.0, 1.0, 0.2)
-        valence = st.slider("Valence :", 0.0, 1.0, 0.2)
+        st.markdown('Acousticness',help = "Acousticness refers to how much of a song or piece of music is made up of natural, organic sounds rather than synthetic or electronic elements. In other words, it's a measure of how 'acoustic' a piece of music sounds. A confidence measure from 0.0 to 1.0, greater value represents higher confidence the track is acoustic.")
+        acousticness = st.slider("Acousticness :", 0.0, 1.0, 0.2, label_visibility = "hidden")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown('Danceability',help = "Danceability is a measure of how suitable a song is for dancing, ranging from 0 to 1. A score of 0 means the song is not danceable at all, while a score of 1 indicates it is highly danceable. This score takes into account factors like tempo, rhythm, beat consistency, and energy, with higher scores indicating stronger, more rhythmically engaging tracks.")
+        danceability = st.slider("Danceability :", 0.0, 1.0, 0.2, label_visibility = "hidden")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown('Energy',help ="Energy in music refers to the intensity and liveliness of a track, with a range from 0 to 1. A score of 0 indicates a very calm, relaxed, or low-energy song, while a score of 1 represents a high-energy, intense track. It’s influenced by elements like tempo, loudness, and the overall drive or excitement in the music.")
+        energy = st.slider("Energy :", 0.0, 1.0, 0.2, label_visibility = "hidden")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown('Instrumentalness',help ="Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.")
+        instrumentalness = st.slider("Instrumentalness :", 0.0, 1.0, 0.2, label_visibility = "hidden")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown('Liveness',help ="Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.")
+        liveness = st.slider("Liveness :", 0.0, 1.0, 0.2, label_visibility = "hidden")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown('Speechiness',help ="Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks.")
+        speechiness = st.slider("Speechiness :", 0.0, 1.0, 0.2, label_visibility = "hidden")
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.markdown('Valence',help ="Valence in music measures the emotional tone or mood of a track, with a range from 0 to 1. A score of 0 indicates a song with a more negative, sad, or dark feeling, while a score of 1 represents a more positive, happy, or uplifting mood. Tracks with a high valence tend to feel joyful or energetic, while those with a low valence may evoke feelings of melancholy or sadness.")
+        valence = st.slider("Valence :", 0.0, 1.0, 0.2, label_visibility = "hidden")
     else:
         acousticness = None
         danceability = None
@@ -81,9 +116,11 @@ def build_params(seeds):
         speechiness = None
         valence = None
 
-    # 1 (Major) or 0 (Minor)
+    # Mode can be 1 (Major) or 0 (Minor)
     options = ["Whatever", "Major", "Minor" ]
-    mode = st.pills("Mode", options)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown('Mode',help = "Mode indicates the modality (major or minor) of a track.")
+    mode = st.pills("Mode", options, label_visibility = "hidden")
     mode_mapping ={
         "Whatever": None,
         "Major": 1,
@@ -91,9 +128,11 @@ def build_params(seeds):
     }
     mode = mode_mapping.get(mode)
 
-    # -1 to 11
+    # Key can be -1 to 11 according pitch class notation
     options = ["Whatever","C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭", "A", "A♯/B♭", "B"]
-    key = st.pills("Key", options)
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown('Key',help = "The key the track is in.")
+    key = st.pills("Key", options, label_visibility = "hidden")
     pitch_class_notation = {
         "Whatever" : None,
         "C": 0,
@@ -111,17 +150,21 @@ def build_params(seeds):
     }
     key = pitch_class_notation.get(key)
 
-    # 0 to 250 (BPM)
+    # Tempo can be 0 to 250 (BPM)
+    st.markdown("<br>", unsafe_allow_html=True)
     enable_tempo = st.toggle("Select track tempo")
     if enable_tempo:
-        tempo = st.slider("Tempo (bpm) :", 1, 250, 1)
+        st.markdown('Tempo (bpm)',help = "Estimated tempo in beats per minute (BPM).")
+        tempo = st.slider("Tempo (bpm) :", 1, 250, 1, label_visibility = "hidden")
     else:
         tempo = None
 
-    # 0 to 100
-    popularity = st.slider("Popularity of the tracks :", 1, 100, 1)
+    # Popularity can be 0 to 100
+    st.markdown("<br>", unsafe_allow_html=True)
+    st.markdown('Popularity',help = "The popularity of the track. The value will be between 0 and 100, with 100 being the most popular. The popularity is calculated by algorithm and is based, in the most part, on the total number of plays the track has had and how recent those plays are. Generally speaking, songs that are being played a lot now will have a higher popularity than songs that were played a lot in the past. Duplicate tracks (e.g. the same track from a single and an album) are rated independently. Artist and album popularity is derived mathematically from track popularity. Note: the popularity value may lag actual popularity by a few days: the value is not updated in real time.")
+    popularity = st.slider("Popularity:", 1, 100, 1, label_visibility = "hidden")
 
-    # Can be 1 to 100
+    # Size is the number of tracks: can be 1 to 100
     # size = st.number_input("Insert a number of track", min_value = 1, max_value = 100)
     size = 1
 
@@ -155,12 +198,11 @@ def build_params(seeds):
 
 
 def get_recommendation(url, recommended_track_params):
-    # Get API response
+    # Get API response Reccobeats
     response = requests.get(url, params=recommended_track_params)
     recommended_track = response.json()
     
     recommended_track_number = len(recommended_track["content"])
-    # st.write(recommended_track_number)
 
     # Create dataframe with recommended tracks
     recommended_track_details = []
@@ -178,16 +220,17 @@ def get_recommendation(url, recommended_track_params):
 
 def display_summary(recommended_track_params):
 
-    # Reverse mapping dictionary
+    # Reverse mapping dictionary to get the labels associated to the int for mode and key
     mode_mapping_inv = {1: "Major", 0: "Minor"}
     pitch_class_notation_inv = {
         0: "C", 1: "C♯/D♭", 2: "D", 3: "D♯/E♭", 4: "E", 5: "F",
         6: "F♯/G♭", 7: "G", 8: "G♯/A♭", 9: "A", 10: "A♯/B♭", 11: "B"
     }
 
-    # Exclude size and seeds from summary
+    # Create dictionary with key/value, eExclude size and seeds from summary
     summary_params = {key: value for key, value in recommended_track_params.items() if key not in ['size', 'seeds']}
 
+    # Display the summary
     for param_name, param_value in summary_params.items():
         # Mode
         if param_name == 'mode':
@@ -231,6 +274,7 @@ def play_track(recommendation):
     track_number = len(recommendation)
     for i in range(track_number):
         track_spotify_URL = recommendation[i]['URL']
+        # Retrieve Id from Spotify URL
         track_spotify_id = re.split("/", track_spotify_URL)[-1]
         track_embed_code = f"""
             <iframe style="border-radius:12px" 
@@ -249,29 +293,44 @@ def play_track(recommendation):
 
 st.header("Let's get your Spotify top tracks", divider = 'green')
 # widget to choose between long / medium / short term
-st.write('The recommended tracks will be generated based on your Spotify top tracks, as well as track features such as danceability or popularity, that you will be able to tune a little further.')
-time_range= st.pills("Spotify listening period:", ['short_term', 'medium_term', 'long_term'])
 
-# Other args
+st.markdown('Spotify time range',help = "Over what time frame the affinities are computed. Valid values: long_term (calculated from ~1 year of data and including all new data as it becomes available), medium_term (approximately last 6 months), short_term (approximately last 4 weeks). Default: medium_term.")
+
+# Define args for get_top_tracks function
+
+time_range_widget= st.pills("Spotify time range", ['Over the last 4 weeks', 'Over the last 6 months', 'Over last year'], label_visibility = "hidden")
+time_range_mapping = {
+    "Over last year" : "long_term",
+    "Over the last 6 months" : "medium_term",
+    "Over the last 4 weeks" : "short_term"
+}
+
+time_range= time_range_mapping.get(time_range_widget)
 track_nb = 5
 offset = 0
 scope = "user-top-read"
 
+# Auth Spotify API
 sp = api_spotify_auth(scope)
 
+# Retrive 5 top tracks IDs (and display in dataframe)
+st.markdown("<br>", unsafe_allow_html=True)
 track_ids = get_top_tracks(time_range, track_nb, offset, sp)
 
 
 # Initialize ags for recommendation
 seeds = track_ids
+# Build params
 recommended_track_params = build_params(seeds)
+
+# Entry points in Reccobeats API
 url = "https://api.reccobeats.com/v1/track/recommendation"
 
 # Display summary of selection
 st.header("Summary of your selection", divider = "green")
 display_summary(recommended_track_params)
 
-# Generate a list a recommended tracks
+# Generate a list a recommended tracks with Reccobeats API
 st.header("Let the magic works :sparkles:", divider = 'green')
 left, middle, right = st.columns(3)
 recommendation = get_recommendation(url, recommended_track_params)
@@ -284,7 +343,7 @@ if middle.button("Generate my track", icon="🎶"):
     # Display players
     play_track(recommendation)
 
-#Save tracks
+#Save tracks (not needed anymore beacuse included in play_track function)
 # left, middle, right = st.columns(3)
 # if middle.button("Save it to my Playlist", icon="💾"):
     
